1160d5e8227cc5662c89c4d033d815c7e7f4c9c6
ChrisFong604 <ccffoonngg@gmail.com>
2023-12-20 14:42:02 -0800

    used pure html to dynamically render header image

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,15 @@
 import Image from "next/image";
-import useScreenType, { ScreenType } from "../Hooks/isMobile";
+import useScreenType, { ScreenType } from "@/Hooks/isMobile";
 
 export default function Header() {
 	const screenType = useScreenType();
 
 	return (
 		<div>
-			{screenType}
-			<Image
-				src={
-					screenType == ScreenType.MOBILE
-						? "/Header/mobile_header.svg"
-						: "/Header/desktop_header.svg"
-				}
-				priority
-				alt="JourneyHacks logo"
-				width={800}
-				height={250}
-			/>
+			<picture className="mt-5">
+				<source media="(max-width: 768px)" srcSet="/Header/mobile_header.svg" />
+				<img src="/Header/desktop_header.svg" alt="JourneyHacks header title" />
+			</picture>
 		</div>
 	);
 }
